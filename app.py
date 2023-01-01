5173289559e27b1fc182115d08437e5137419ebf
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2023-01-01 23:12:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.write(
 
 st.title('ANDRUP')
 st.header('Automated Normal Distribution Ranking Using Python ')
-st.markdown("""This web application can automatically compute the mean, variance, and standard deviation of data within the given MS excel Sheet, Google Sheets, or Inputted Values. ANDRUP will also determine the z-score, area, and probability percentage of given data points by a user within the dataset supported by a downloadable standard normal distribution curve. ANDRUP also provides ranking analysis based on actual percentile rank and percentile rank determined by the normal distribution model. This web application aims to lessen the time and burden when computing and analyzing with standard normal distribution..
+st.markdown("""This web application can automatically compute the mean, variance, and standard deviation of data within the given MS excel Sheet, Google Sheets, or Inputted Values. ANDRUP will also determine the z-score, area, and probability percentage of given data points by a user within the dataset supported by a downloadable standard normal distribution curve. ANDRUP also provides ranking analysis based on actual percentile rank and percentile rank determined by the normal distribution model. This web application aims to lessen the time and burden when computing and analyzing with standard normal distribution.
             """)
 
 st.markdown('---')
